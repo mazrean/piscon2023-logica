012303bf38d8d854598086bfc5c3beda4ba35938
mazrean <shunsuke.w04@gmail.com>
2023-03-08 05:52:15 +0900

    try lock

--- app/go/main.go
+++ app/go/main.go
@@ -893,6 +893,10 @@ var (
 		Name: "post_wait_count",
 		Help: "The total number of waiting post requests",
 	})
+	postQueueCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "post_queue_count",
+		Help: "The total number of queued post requests",
+	})
 	cond         = sync.NewCond(&sync.Mutex{})
 	postBookChan = make(chan Book, 10000)
 	postBookLock = sync.RWMutex{}
@@ -914,7 +918,9 @@ func initPostBookChan() {
 				thisEnd  *bool
 			)
 			func() {
-				postBookLock.Lock()
+				for !postBookLock.TryLock() {
+					time.Sleep(1 * time.Millisecond)
+				}
 				defer postBookLock.Unlock()
 
 				thisChan = postBookChan
