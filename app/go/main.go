d6ca3fc290e45acb5d5fd2796e5328810726cf89
mazrean <shunsuke.w04@gmail.com>
2023-03-08 18:18:23 +0900

    Revert "戻す"
    This reverts commit de153bf25bda1b6278f59e4752a1990b150335e7.

--- app/go/main.go
+++ app/go/main.go
@@ -369,10 +369,7 @@ func (j pngEncoder) Encode(w io.Writer, img image.Image) error {
 
 // QRコードを生成
 func generateQRCode(id string, w io.Writer) error {
-	encryptedID, err := encrypt(id)
-	if err != nil {
-		return err
-	}
+	encryptedID := id
 
 	/*
 		生成するQRコードの仕様
@@ -383,9 +380,6 @@ func generateQRCode(id string, w io.Writer) error {
 	*/
 	cmd := exec.
 		Command("qrencode", "-o", "-", "-t", "PNG", "-s", "1", "-v", "6", "--strict-version", "-l", "M")
-	if err != nil {
-		return err
-	}
 	cmd.Stdin = strings.NewReader(encryptedID)
 	r, err := cmd.StdoutPipe()
 	if err != nil {
