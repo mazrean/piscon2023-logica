7b0403b2025a11c681f57bbadd9eeb987df9026e
mazrean <shunsuke.w04@gmail.com>
2023-03-08 18:18:33 +0900

    Revert "fix"
    This reverts commit 783c3c4a5d0e8ac613ba133bb11183a52a9cfe8f.

--- app/go/main.go
+++ app/go/main.go
@@ -987,14 +987,12 @@ func postBooksHandler(c echo.Context) error {
 	}
 
 	bookValues := make([]*isulocker.Value[GetBookResponse], 0, len(res))
-	for _, book := range res {
-		bookValue := isulocker.NewValue(GetBookResponse{
-			Book:    book,
-			Lending: false,
-		}, "book")
-		bookValues = append(bookValues, bookValue)
-	}
 	bookSliceCache.Append(bookValues...)
+	for _, book := range bookValues {
+		book.Read(func(b *GetBookResponse) {
+			bookCache.Store(b.ID, book)
+		})
+	}
 
 	return c.JSON(http.StatusCreated, res)
 }
